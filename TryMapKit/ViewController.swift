a1751d5e627137da38bbc2852b9efc5be3a93673
Sayyid Maulana Khakul Yakin <sayyid.yakin@students.amikom.ac.id>
2019-12-29 10:31:30 +0700

    feature(addAnotation) just trying mapkit

--- TryMapKit/ViewController.swift
+++ TryMapKit/ViewController.swift
@@ -7,11 +7,26 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapKit: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        // step 1
+        mapKit.mapType = .hybrid
+        let location = CLLocationCoordinate2DMake(-6.121435, 106.774124)
+        // setp 2
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapKit.setRegion(region, animated: true)
+        // step 4
+        let anotation = MKPointAnnotation()
+        anotation.coordinate = location
+        anotation.title = "Jakarta"
+        anotation.subtitle = "Jakarta Utara"
+        mapKit.addAnnotation(anotation)
         // Do any additional setup after loading the view.
     }
 
